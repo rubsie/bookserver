8ee5e1c168affca54ab1bc2b6e1442660990c1cd
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2020-11-19 14:59:53 +0100

    add logging

--- src/main/java/be/thomasmore/bookserver/controllers/BookController.java
+++ src/main/java/be/thomasmore/bookserver/controllers/BookController.java
@@ -1,6 +1,8 @@
 package be.thomasmore.bookserver.controllers;
 
 import be.thomasmore.bookserver.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,12 @@ import static java.lang.Thread.sleep;
 
 @RestController
 public class BookController {
+    Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @CrossOrigin
     @GetMapping("/books")
     public List<Book> findAll() {
+        logger.info("##### findAll");
         Book[] books = {new Book("MaddAddam"),
                 new Book("The year of the flood"),
                 new Book("Oryx and Crake")};
