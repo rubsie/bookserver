7e70e0b203f35582f2ca83a1d155072cf47156e8
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2020-11-30 15:49:02 +0100

    optimize imports

--- src/main/java/be/thomasmore/bookserver/controllers/BookController.java
+++ src/main/java/be/thomasmore/bookserver/controllers/BookController.java
@@ -5,14 +5,9 @@ import be.thomasmore.bookserver.repositories.BookRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-
-import static java.lang.Thread.sleep;
 
 @RestController
 public class BookController {
