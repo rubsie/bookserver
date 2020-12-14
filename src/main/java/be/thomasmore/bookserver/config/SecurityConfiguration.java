2ff4c02753a5d5061390f4f8bac80b68e838b0a3
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2020-12-14 20:07:34 +0100

    add csrf protection

--- src/main/java/be/thomasmore/bookserver/config/SecurityConfiguration.java
+++ src/main/java/be/thomasmore/bookserver/config/SecurityConfiguration.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //http.csrf().disable(); DOE DIT ZEKER NIET!!!!
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         http.authorizeRequests().anyRequest().authenticated();
         http.httpBasic();
         //http.formLogin();
