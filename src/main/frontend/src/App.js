807cb589c9a967ada2be31425e69c7da1a908c85
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-06-20 19:13:40 +0200

    context: AuthenticationContext: move authenticate functin to context

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -28,7 +28,7 @@ function ProvidedApp() {
     const [selectedBook, setSelectedBook] = useState();
     const [showCreateForm, setShowCreateForm] = useState(false);
     const {setMessage, setIsLoading} = useMessageContext();
-    const {username, setUsername, showLoginBox, setShowLoginBox} = useAuthenticationContext();
+    const {username, setUsername, showLoginBox, setShowLoginBox, authenticate} = useAuthenticationContext();
 
 
     console.log("render App()");
@@ -155,32 +155,6 @@ function ProvidedApp() {
         setIsLoading(false);
     }
 
-    async function authenticate(username, password) {
-        console.log(`   async authenticate: start ${username}`);
-        setIsLoading(true);
-        try {
-            const fetchOptions = {
-                method: 'GET',
-                'credentials': 'include',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8',
-                    'X-Requested-With': 'XMLHttpRequest',
-                    authorization: "Basic " + window.btoa(`${username}:${password}`)
-                },
-            };
-            const response = await fetch(`/api/authenticate`, fetchOptions);
-            const body = await response.json();
-            console.log(`   async authenticate: received response ${JSON.stringify(body)}`);
-            setUsername(body.username);
-            setMessage();
-            setShowLoginBox(false);
-            console.log("   async authenticate: done");
-        } catch (e) {
-            console.log(`   async authenticate: ERROR ${JSON.stringify(e)}`);
-            setMessage("Login error");
-        }
-        setIsLoading(false);
-    }
 
     async function refreshAuthentication() {
         console.log(`   async refreshAuthentication: start`);
--- src/main/frontend/src/contexts/authenticationcontext.js
+++ src/main/frontend/src/contexts/authenticationcontext.js
@@ -1,4 +1,5 @@
-import React, {createContext, useContext, useMemo, useState} from 'react';
+import React, {createContext, useContext, useMemo, useState, useCallback} from 'react';
+import {useMessageContext} from "./messagecontext";
 
 const AuthenticationContext = createContext();
 
@@ -6,10 +7,38 @@ const AuthenticationContext = createContext();
 export function AuthenticationProvider(props) {
     const [username, setUsername] = useState();
     const [showLoginBox, setShowLoginBox] = useState(false);
+    const {setMessage, setIsLoading} = useMessageContext();
+
+    const authenticate = useCallback(async (username, password) => {
+        console.log(`   async authenticate: start ${username}`);
+        setIsLoading(true);
+        try {
+            const fetchOptions = {
+                method: 'GET',
+                'credentials': 'include',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    authorization: "Basic " + window.btoa(`${username}:${password}`)
+                },
+            };
+            const response = await fetch(`/api/authenticate`, fetchOptions);
+            const body = await response.json();
+            console.log(`   async authenticate: received response ${JSON.stringify(body)}`);
+            setUsername(body.username);
+            setMessage();
+            setShowLoginBox(false);
+            console.log("   async authenticate: done");
+        } catch (e) {
+            console.log(`   async authenticate: ERROR ${JSON.stringify(e)}`);
+            setMessage("Login error");
+        }
+        setIsLoading(false);
+    }, [setIsLoading, setUsername, setMessage, setShowLoginBox]);
 
     const api = useMemo(() => ({
-            username, setUsername
-        }), [username, setUsername]
+            username, setUsername, showLoginBox, setShowLoginBox, authenticate,
+        }), [username, setUsername, showLoginBox, setShowLoginBox, authenticate]
     );
 
     return (
