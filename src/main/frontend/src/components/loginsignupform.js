9b79197d8e1bd783180024ecca3406ade9c407f7
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-06-23 22:03:37 +0200

    first try to cleanup - all ok so far except the autofocus

--- src/main/frontend/src/components/loginsignupform.js
+++ src/main/frontend/src/components/loginsignupform.js
@@ -7,8 +7,79 @@ import {Message} from "./message";
 import {LoginLink, SignupLink} from "./authbuttons";
 import {useMessageContext} from "../contexts/messagecontext";
 
+function ModalWithForm(props) {
+    const {tempObject, title, isOpen, close, doSubmit} = props;
+    const {setMessage, clearAllMessages} = useMessageContext();
+    const firstInputRefElement = useRef(null);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log(`SUBMIT ModalWithForm ${title}`);
+        const result = await doSubmit(tempObject);
+        if (result) close()
+        else if (firstInputRefElement.current) firstInputRefElement.current.focus();
+    }
+
+    useEffect(() => {
+        //put focus on first input element when the form becomes visible
+        if (isOpen) {
+            if (firstInputRefElement.current) firstInputRefElement.current.focus();
+            clearAllMessages();
+            setMessage(<>You don't have an account? Then <SignupLink/>.</>);
+        }
+    }, [isOpen]);
+
+    if (!isOpen) return null;
+
+    return <>
+        <Modal.Header closeButton>
+            <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={e => handleSubmit(e)}>
+            <Modal.Body>
+                <Message/>
+                {props.children}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={close}>cancel</Button>
+                <Button variant="primary" type="submit">login</Button>
+            </Modal.Footer>
+        </Form></>;
+}
+
+function useFormObject(initialObject) {
+    const [tempObject, setTempObject] = useState(initialObject);
+    return tempObject;
+}
 
 function LoginFormContent(props) {
+    const {close} = props;
+    const [tempObject, setTempObject] = useState({username: "", password: ""});
+    const firstInputRefElement = useRef(null);
+    const {showLoginBox, authenticate} = useAuthenticationContext();
+
+    return <ModalWithForm tempObject={tempObject}
+                          title="Log in"
+                          isOpen={showLoginBox}
+                          close={close}
+                          doSubmit={async (tempObject) => await authenticate(tempObject.username, tempObject.password)}>
+        <Form.Group controlId="username">
+            <Form.Label>username: </Form.Label>
+            <Form.Control required placeholder="Enter username" autoComplete="username"
+                          ref={firstInputRefElement}
+                          onChange={e => setTempObject({...tempObject, username: e.target.value})}/>
+
+        </Form.Group>
+        <Form.Group controlId="password">
+            <Form.Label>password: </Form.Label>
+            <Form.Control type="password" required placeholder="Password" autoComplete="current-password"
+                          onChange={e => setTempObject({...tempObject, password: e.target.value})}/>
+        </Form.Group>
+
+    </ModalWithForm>;
+}
+
+function LoginFormContentOrig(props) {
     const {close} = props;
     const [tempObject, setTempObject] = useState({username: "", password: ""});
     const {showLoginBox, authenticate} = useAuthenticationContext();
