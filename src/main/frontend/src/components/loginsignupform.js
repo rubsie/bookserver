b5fc1e37ee2275c317d8d2febf462003ee9749e0
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-06-23 22:28:30 +0200

    first try to cleanup further - all still ok

--- src/main/frontend/src/components/loginsignupform.js
+++ src/main/frontend/src/components/loginsignupform.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
@@ -8,7 +8,8 @@ import {LoginLink, SignupLink} from "./authbuttons";
 import {useMessageContext} from "../contexts/messagecontext";
 
 function ModalWithForm(props) {
-    const {tempObject, firstInputRefElement, title, isOpen, close, doSubmit} = props;
+    const {objectForModalWithForm, title, isOpen, close, doSubmit, initialMessage} = props;
+    const {tempObject, firstInputRefElement} = objectForModalWithForm;
     const {setMessage, clearAllMessages} = useMessageContext();
 
     async function handleSubmit(e) {
@@ -24,7 +25,7 @@ function ModalWithForm(props) {
         if (isOpen) {
             if (firstInputRefElement.current) firstInputRefElement.current.focus();
             clearAllMessages();
-            setMessage(<>You don't have an account? Then <SignupLink/>.</>);
+            setMessage(initialMessage);
         }
     }, [isOpen]);
 
@@ -41,41 +42,53 @@ function ModalWithForm(props) {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={close}>cancel</Button>
-                <Button variant="primary" type="submit">login</Button>
+                <Button variant="primary" type="submit">{title}</Button>
             </Modal.Footer>
         </Form></>;
 }
 
-function useFormObject(initialObject) {
+function useModalWithFormProps(initialObject) {
     const [tempObject, setTempObject] = useState(initialObject);
-    return tempObject;
+    const firstInputRefElement = useRef(null);
+
+    const onChange = useCallback((e, fieldName) => {
+        const newTempObject = {...tempObject};
+        newTempObject[fieldName] = e.target.value;
+        setTempObject(newTempObject);
+    }, [tempObject, setTempObject]);
+
+    return {tempObject, setTempObject, firstInputRefElement, onChange};
 }
 
+//the new one
 function LoginFormContent(props) {
     const {close} = props;
-    const [tempObject, setTempObject] = useState({username: "", password: ""});
-    const firstInputRefElement = useRef(null);
+    const modalWithFormProps = useModalWithFormProps({username: "", password: ""});
+    const {firstInputRefElement, onChange} = modalWithFormProps;
     const {showLoginBox, authenticate} = useAuthenticationContext();
 
-    return <ModalWithForm tempObject={tempObject}
-                          firstInputRefElement={firstInputRefElement}
+    async function doSubmit(tempObject) {
+        return await authenticate(tempObject.username, tempObject.password);
+    }
+
+    return <ModalWithForm objectForModalWithForm={modalWithFormProps}
                           title="Log in"
                           isOpen={showLoginBox}
                           close={close}
-                          doSubmit={async (tempObject) => await authenticate(tempObject.username, tempObject.password)}>
+                          doSubmit={doSubmit}
+                          initialMessage={<>You don't have an account? Then <SignupLink/>.</>}>
         <Form.Group controlId="username">
             <Form.Label>username: </Form.Label>
             <Form.Control required placeholder="Enter username" autoComplete="username"
                           ref={firstInputRefElement}
-                          onChange={e => setTempObject({...tempObject, username: e.target.value})}/>
+                          onChange={e => onChange(e, "username")}/>
 
         </Form.Group>
         <Form.Group controlId="password">
             <Form.Label>password: </Form.Label>
             <Form.Control type="password" required placeholder="Password" autoComplete="current-password"
-                          onChange={e => setTempObject({...tempObject, password: e.target.value})}/>
+                          onChange={e => onChange(e, "password")}/>
         </Form.Group>
-
     </ModalWithForm>;
 }
 
