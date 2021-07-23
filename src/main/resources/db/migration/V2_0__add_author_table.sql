4448bfa59af02acaa0121e0a9c0461339c659d42
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-23 11:38:47 +0200

    try out flyway migration script on local postgres and fix issue for postgres

--- src/main/resources/db/migration/V2_0__add_author_table.sql
+++ src/main/resources/db/migration/V2_0__add_author_table.sql
@@ -15,7 +15,7 @@ create table book_authors(
 
 /*convert the data that is already in the author-column of the book-table*/
 insert into author (ID, NAME)
-select nextval('AUTHOR_SEQ'), author  from (SELECT distinct author FROM BOOK);
+select nextval('AUTHOR_SEQ'), author  from (SELECT distinct author FROM BOOK) as author;
 
 insert into BOOK_AUTHORS (books_id, authors_id)
 SELECT book.id, author.id FROM book inner join AUTHOR  on book.author=author.name;
