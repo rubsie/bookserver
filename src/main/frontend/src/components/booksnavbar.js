14082a3bdca27659b7e9da80626e23255e081b57
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-25 17:06:21 +0200

    add react-bootstrap again

--- src/main/frontend/src/components/booksnavbar.js
+++ src/main/frontend/src/components/booksnavbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {useAuthenticationContext} from "../contexts/authenticationcontext";
 import {LoginNavLink, LogoutNavLink} from "./authbuttons";
-import {MDBBtn, MDBContainer, MDBNavbar, MDBNavbarBrand, MDBNavbarItem, MDBNavbarNav} from 'mdb-react-ui-kit';
 import {MdAdd, MdRefresh} from "react-icons/md";
 import {IfLoggedIn} from "./ifLoggedIn";
 import {useBooksContext} from "../contexts/bookscontext";
+import {Button, Container, Nav, Navbar} from "react-bootstrap";
 
 /** * @return {null} */
 export function Booksnavbar(props) {
@@ -13,19 +13,23 @@ export function Booksnavbar(props) {
     const {getBooks} = useBooksContext();
     const userInfo = username ? `logged in as ${username}` : "not logged in.";
 
-    return <MDBNavbar dark bgColor='dark' sticky="top" className='p-2'>
-        <MDBNavbarBrand>{userInfo}</MDBNavbarBrand>
-        <MDBNavbarNav left fullWidth={false}>
-            <MDBContainer fluid>
-                <MDBBtn className="m-1"  size='sm' color="light" onClick={getBooks}><MdRefresh color="inherit"/></MDBBtn>
+    return <Navbar fixed="top" bg="dark" variant="dark" className="shadow-sm">
+        <Navbar.Brand>{userInfo}</Navbar.Brand>
+        <Container>
+            <Nav>
+                <Button className="m-1" size='sm' color="light"
+                        onClick={getBooks}>
+                    <MdRefresh color="inherit"/></Button>
                 <IfLoggedIn>
-                    <MDBBtn className="m-1"  size='sm' color="light" onClick={() => setShowCreateForm(true)}><MdAdd color="inherit"/></MDBBtn>
+                    <Button className="m-1" size='sm' color="light"
+                            nClick={() => setShowCreateForm(true)}>
+                        <MdAdd color="inherit"/></Button>
                 </IfLoggedIn>
-            </MDBContainer>
-        </MDBNavbarNav>
-        <MDBNavbarNav right fullWidth={false}>
-            <MDBNavbarItem><LoginNavLink/></MDBNavbarItem>
-            <MDBNavbarItem><LogoutNavLink/></MDBNavbarItem>
-        </MDBNavbarNav>
-    </MDBNavbar>;
+            </Nav>
+            <Nav className="justify-content-end">
+                <Nav.Item><LoginNavLink/></Nav.Item>
+                <Nav.Item><LogoutNavLink/></Nav.Item>
+            </Nav>
+        </Container>
+    </Navbar>;
 }
