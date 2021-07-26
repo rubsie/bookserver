e1bbf9c8c333d701f73b95f6cd9df3b306e2145b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-26 10:26:15 +0200

    add react-bootstrap again - createform

--- src/main/frontend/src/components/createform.js
+++ src/main/frontend/src/components/createform.js
@@ -2,15 +2,14 @@ import React from "react";
 import {useAuthenticationContext} from "../contexts/authenticationcontext";
 import {useBooksContext} from "../contexts/bookscontext";
 import {useAuthorsContext} from "../contexts/authorscontext";
-import {ModalMdbWithForm, usePropsForModalMdbWithInitialObject} from "./modalMdb";
-import {MDBInput} from "mdb-react-ui-kit";
-import {MySelectMultiple} from "./select";
+import {ModalWithForm, usePropsForModalWithInitialObject} from "./modal";
+import {Form} from "react-bootstrap";
 
 export function CreateForm(props) {
     const {show, close} = props;
     const {isLoggedIn} = useAuthenticationContext();
     const {createBookWithAuthors} = useBooksContext();
-    const modalWithFormProps = usePropsForModalMdbWithInitialObject({title: "", authorIds: [], priceInEur: ""});
+    const modalWithFormProps = usePropsForModalWithInitialObject({title: "", authorIds: [], priceInEur: ""});
     const {tempObject, firstInputRefElement, onChange, onChangeNumber, onChangeSelect} = modalWithFormProps;
     const {authors} = useAuthorsContext()
 
@@ -21,24 +20,29 @@ export function CreateForm(props) {
 
     console.log(`CreateForm`, {tempObject});
     /* TODO create book with new author  */
-    return <ModalMdbWithForm modalWithFormProps={modalWithFormProps}
-                             title="New book"
-                             isOpen={isLoggedIn && show}
-                             close={close}
-                             doSubmit={doSubmit}
-                             saveButtonText={"save"}>
-
-        <MDBInput className="mt-2" label="title" required value={tempObject && tempObject.title}
-                  inputRef={firstInputRefElement}
-                  onChange={e => onChange(e, "title")}/>
-
-        <MySelectMultiple value={tempObject && tempObject.authorIds}
+    return <ModalWithForm modalWithFormProps={modalWithFormProps}
+                          title="New book"
+                          isOpen={isLoggedIn && show}
+                          close={close}
+                          doSubmit={doSubmit}
+                          saveButtonText={"save"}>
+        <Form.Group controlId="title" className="mb-3">
+            <Form.Label>title: </Form.Label>
+            <Form.Control required value={tempObject && tempObject.title}
+                          ref={firstInputRefElement}
+                          onChange={e => onChange(e, "title")}/>
+        </Form.Group>
+        <Form.Group controlId="authorIds" className="mb-3">
+            <Form.Label>authors: </Form.Label>
+            <Form.Control as="select" multiple required value={tempObject && tempObject.authors}
                           onChange={e => onChangeSelect(e, "authorIds")}>
-            {authors.map(a => <option key={a.id} value={a.id}>{a.name}</option>)}
-        </MySelectMultiple>
-        <MDBInput className="mt-2" label="price (€)" type="number" min="0" max="2000"
-                  value={tempObject ? String(tempObject.priceInEur) : ""}
-                  onChange={e => onChangeNumber(e, "priceInEur")}/>
-
-    </ModalMdbWithForm>;
+                {authors.map(a => <option key={a.id} value={a.id}>{a.name}</option>)}
+            </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="price" className="mb-3">
+            <Form.Label>price (€): </Form.Label>
+            <Form.Control value={tempObject && tempObject.priceInEur} type="number" min="0" max="2000"
+                          onChange={e => onChangeNumber(e, "priceInEur")}/>
+        </Form.Group>
+    </ModalWithForm>;
 }
