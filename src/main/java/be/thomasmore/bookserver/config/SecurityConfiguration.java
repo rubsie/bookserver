c8e4a53fb51bce7408929c358f46319feb49bb0d
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-09-01 13:04:15 +0200

    versie 3: authentication

--- src/main/java/be/thomasmore/bookserver/config/SecurityConfiguration.java
+++ src/main/java/be/thomasmore/bookserver/config/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //http.csrf().disable(); DOE DIT ZEKER NIET!!!!
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        http.authorizeRequests().antMatchers("/api/genre/**").authenticated();
+        http.authorizeRequests().antMatchers("/api/genres/**").authenticated();
         http.authorizeRequests().antMatchers("/api/authenticate/**").authenticated();
         http.authorizeRequests().antMatchers("/api/signup/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll();
