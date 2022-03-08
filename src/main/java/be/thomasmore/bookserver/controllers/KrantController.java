b889ca029c261068f99135e5266c3a2cf3ff8a82
Ronny Wouters <ozzy.wouters@telenet.be>
2022-03-08 20:13:07 +0100

    Query in repository en request handler voor kranten met oplage in bereik

--- src/main/java/be/thomasmore/bookserver/controllers/KrantController.java
+++ src/main/java/be/thomasmore/bookserver/controllers/KrantController.java
@@ -4,9 +4,13 @@ import be.thomasmore.bookserver.model.Krant;
 import be.thomasmore.bookserver.repositories.KrantRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @Slf4j
 @RestController
@@ -19,5 +23,14 @@ public class KrantController {
     public  Iterable<Krant> findAll(){
         log.info("##### findAll kranten");
         return krantRepository.findAll();
+    }
+
+    @GetMapping("/oplage")
+    @Transactional
+    public Iterable<Krant> findKrantenByOplageInBereik(@RequestParam(required = false) Long min,
+                                                       @RequestParam(required = false) Long max) {
+        log.info("##### findKrantenByOplageInBereik -- min=" + min);
+        log.info("##### findKrantenByOplageInBereik -- max=" + max);
+        return krantRepository.findByOplageInBereik(min,max);
     }
 }
--- src/main/java/be/thomasmore/bookserver/repositories/KrantRepository.java
+++ src/main/java/be/thomasmore/bookserver/repositories/KrantRepository.java
@@ -1,9 +1,13 @@
 package be.thomasmore.bookserver.repositories;
 
 import be.thomasmore.bookserver.model.Krant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KrantRepository extends CrudRepository<Krant,Integer> {
+    @Query("SELECT k FROM Krant k WHERE (:min IS NULL OR k.oplage >= :min) AND (:max IS NULL OR k.oplage <= :max) ORDER BY k.oplage")
+    Iterable<Krant> findByOplageInBereik(@Param("min") Long min, @Param("max") Long max);
 }
